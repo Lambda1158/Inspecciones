6d6defc3af4729b7bb9e9fa09a93770c739d108b
bruno <brunoherrera1158@gmail.com>
2022-06-14 17:53:09 -0300

    fill

---
+++ client/src/components/prueba/test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./test.css"
+export default function Test(){
+
+
+    return(
+        <div class="grid">
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>
+  <div>item</div>  
+</div>
+    )
+}
