7c9b73455689b161b78702a32ef41464ec0d8f88
bruno <brunoherrera1158@gmail.com>
2022-06-22 17:58:10 -0300

    fill

--- client/src/components/prueba/test.js
+++ client/src/components/prueba/test.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./test.css"
-export default function Test(contenido){
-    let a=contenido.contenido
+export default function Test({ contenido }) {
 
-    return(
-           <div class="item">{a}</div>
+    return (
+        <div class="item">
+            <h3>{contenido}</h3>
+            <button className="btn">click</button>
+        </div>
     )
 }
